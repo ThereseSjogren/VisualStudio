83c8af018c8e0360aa262a0f8bd7cc2e30b2d134
scoophamp <hampus_cousin@hotmailcom>
2016-02-24 10:17:54 +0100

    Prop

---
+++ TestAvVSOchGIT/TestAvVSOchGIT/Atom.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAvVSOchGIT
+{
+    class Atom
+    {
+        private int Kula;
+
+        public int kula
+        {
+            get { return Kula; }
+            set { Kula = value; }
+        }
+        private int Antal;
+
+        public int antal
+        {
+            get { return Antal; }
+            set { Antal = value; }
+        }
+
+    }
+}
